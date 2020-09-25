53254fc9cf7b1c7809bf3d263fe3746f8f62a068
Gerald Pape <mail@geraldpape.io>
2020-09-25 15:21:01 +0200

    read postgres host from environment variable

--- api_test/db.py
+++ api_test/db.py
@@ -5,16 +5,9 @@ import dotenv
 from sqlalchemy import create_engine
 
 
-def get_db_host():
-    if Path("/.dockerenv").exists():
-        return "postgres"
-    else:
-        return "localhost"
-
-
 def get_db_engine():
     dotenv.load_dotenv()
     pw = os.getenv("POSTGRES_PASSWORD")
-    host = get_db_host()
+    host = os.getenv("POSTGRES_HOST", "localhost")
     engine = create_engine(f"postgresql+psycopg2://postgres:{pw}@{host}:5432/postgres")
     return engine
